2fbca2848e1b75474ad7c43fba86ec07ecadbfd5
Mastani-ep <eparuch@student.agh.edu.pl>
2023-01-04 02:59:26 +0100

    Finishing the search form template

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,52 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, RadioField, IntegerField
 from wtforms.validators import InputRequired, EqualTo
+from flask_wtf.file import FileField, FileRequired
+from wtforms.widgets import NumberInput
 
 
 class SearchForm(FlaskForm):
-    first_query = StringField('first_query', validators=[InputRequired(message='This field is required.')],
+    first_query = StringField('1st item', validators=[InputRequired(message='This field is required.')],
                               id='firstQueryInput')
+    second_query = StringField('2nd item', id='secondQueryInput')
+    third_query = StringField('3rd item', id='thirdQueryInput')
+    fourth_query = StringField('4th item', id='fourthQueryInput')
+    fifth_query = StringField('5th item', id='fifthQueryInput')
+    sixth_query = StringField('6th item', id='sixthQueryInput')
+    seventh_query = StringField('7th item', id='seventhQueryInput')
+    eighth_query = StringField('8th item', id='eighthQueryInput')
+    ninth_query = StringField('9th item', id='ninthQueryInput')
+    tenth_query = StringField('10th item', id='tenthQueryInput')
+
+    first_amount = IntegerField('Amount', widget=NumberInput(min=0, step=1), default=1)
+    second_amount = IntegerField('Amount', widget=NumberInput(min=0, step=1), default=1)
+    third_amount = IntegerField('Amount', widget=NumberInput(min=0, step=1), default=1)
+    fourth_amount = IntegerField('Amount', widget=NumberInput(min=0, step=1), default=1)
+    fifth_amount = IntegerField('Amount', widget=NumberInput(min=0, step=1), default=1)
+    sixth_amount = IntegerField('Amount', widget=NumberInput(min=0, step=1), default=1)
+    seventh_amount = IntegerField('Amount', widget=NumberInput(min=0, step=1), default=1)
+    eighth_amount = IntegerField('Amount', widget=NumberInput(min=0, step=1), default=1)
+    ninth_amount = IntegerField('Amount', widget=NumberInput(min=0, step=1), default=1)
+    tenth_amount = IntegerField('Amount', widget=NumberInput(min=0, step=1), default=1)
+
+    # filters
+    platform = RadioField('Platform', choices=[('Ceneo', 'Ceneo'), ('Allegro', 'Allegro (not available)'),
+                                               ('C&A', 'Ceneo&Allegro (not available)')], default='Ceneo')
+    price_min = IntegerField('Price from', widget=NumberInput(min=0, step=1), render_kw={"placeholder": "PLN"})
+    price_max = IntegerField('Price to', widget=NumberInput(min=0, step=1), render_kw={"placeholder": "PLN"})
+    stores_max = IntegerField('Maximum number of stores to buy from', widget=NumberInput(min=0, step=1))
+    submit = SubmitField('Search', id='searchButton')
+
+
+class SearchFormFile(FlaskForm):
+    file = FileField('Shopping list', name="file", validators=[FileRequired()])
+
+    # filters
+    platform = RadioField('Platform', choices=[('Ceneo', 'Ceneo'), ('Allegro', 'Allegro (not available)'),
+                                               ('C&A', 'Ceneo&Allegro (not available)')], default='Ceneo')
+    price_min = IntegerField('Price from', widget=NumberInput(min=0, step=1), render_kw={"placeholder": "PLN"})
+    price_max = IntegerField('Price to', widget=NumberInput(min=0, step=1), render_kw={"placeholder": "PLN"})
+    stores_max = IntegerField('Maximum number of stores to buy from', widget=NumberInput(min=0, step=1))
     submit = SubmitField('Search', id='searchButton')
 
 
--- app/views/home.py
+++ app/views/home.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request
 from flask_login import current_user
 
-from ..forms import SearchForm
+from ..forms import SearchForm, SearchFormFile
 
 bp = Blueprint('bp_home', __name__)
 
@@ -9,11 +9,51 @@ bp = Blueprint('bp_home', __name__)
 @bp.route('/')
 def home_get():
     form = SearchForm()
-    return render_template('home.html', form=form, user=current_user)
+    form_file = SearchFormFile()
+    return render_template('home.html', form=form, form_file=form_file, user=current_user)
 
 
 @bp.route('/', methods=['POST'])
 def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+    #queries
+    queries = []
+    amounts = []
+    if 'first_query' in request.form and request.form['first_query'] != '':
+        queries.append(request.form['first_query'].upper())
+        amounts.append(request.form['first_amount'])
+    if 'second_query' in request.form and request.form['second_query'] != '':
+        queries.append(request.form['second_query'].upper())
+        amounts.append(request.form['second_amount'])
+    if 'third_query' in request.form and request.form['third_query'] != '':
+        queries.append(request.form['third_query'].upper())
+        amounts.append(request.form['third_amount'])
+    if 'fourth_query' in request.form and request.form['fourth_query'] != '':
+        queries.append(request.form['fourth_query'].upper())
+        amounts.append(request.form['fourth_amount'])
+    if 'fifth_query' in request.form and request.form['fifth_query'] != '':
+        queries.append(request.form['fifth_query'].upper())
+        amounts.append(request.form['fifth_amount'])
+    if 'sixth_query' in request.form and request.form['sixth_query'] != '':
+        queries.append(request.form['sixth_query'].upper())
+        amounts.append(request.form['sixth_amount'])
+    if 'seventh_query' in request.form and request.form['seventh_query'] != '':
+        queries.append(request.form['seventh_query'].upper())
+        amounts.append(request.form['seventh_amount'])
+    if 'eighth_query' in request.form and request.form['eighth_query'] != '':
+        queries.append(request.form['eighth_query'].upper())
+        amounts.append(request.form['eighth_amount'])
+    if 'ninth_query' in request.form and request.form['ninth_query'] != '':
+        queries.append(request.form['ninth_query'].upper())
+        amounts.append(request.form['ninth_amount'])
+    if 'tenth_query' in request.form and request.form['tenth_query'] != '':
+        queries.append(request.form['tenth_query'].upper())
+        amounts.append(request.form['tenth_amount'])
+
+    #filters
+    filters = [request.form['platform'].upper(), request.form['price_min'].upper(), request.form['price_max'].upper(),
+               request.form['stores_max'].upper()]
+
+    filename = None
+    if 'file' in request.files:
+        filename = request.files['file'].filename
+    return render_template('search_results.html', user=current_user, queries=queries, amounts = amounts, filters=filters, filename=filename)
