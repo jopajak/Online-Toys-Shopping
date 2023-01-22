c0b18b88ad56fcf428ac50b6a8e80e61126596d1
Corqi <kowalikm@student.agh.edu.pl>
2023-01-22 23:17:50 +0100

    Updated ceneo_scrapper unit tests

--- app/tests/test_ceneo_scrapper.py
+++ app/tests/test_ceneo_scrapper.py
@@ -4,10 +4,10 @@ from app import ceneo_scrapper
 def test_get_list_of_products():
     # list of product names to test
     product_exist_list = ["Buty", "Klocki", "Lego", "Płetwy", "Obroża dla psa"]
-    product_nonexist_list = ["hadshsakhsf", "", "a"]
+    product_nonexist_list = ["hadshsakhsf", "sasdasdadsa", "xasdasdsa"]
 
     for product in product_exist_list:
-        assert 10 == len(ceneo_scrapper.get_list_of_products(product, False))
+        assert 0 != len(ceneo_scrapper.get_list_of_products(product, False))
 
     for product in product_nonexist_list:
         assert 0 == len(ceneo_scrapper.get_list_of_products(product, False))
@@ -16,3 +16,11 @@ def test_get_list_of_products():
 def test_get_url():
     # test empty id
     assert [] == ceneo_scrapper.get_url("")
+
+    # list of couple item id
+    product_id_list = ["81329691", "35973009", "94823130"]
+
+    # test if url will output any offers
+    for i in range(5):
+        for product_id in product_id_list:
+            assert 0 != len(ceneo_scrapper.get_url(product_id))
